89b64224af54f398f145df898f45f4edf6a17a1d
Ryan Barnes <rybarnes@gmail.com>
2021-01-30 16:56:27 -0500

    form

--- gatsby/src/lib/formium.js
+++ gatsby/src/lib/formium.js
@@ -1,3 +1,3 @@
 import { createClient } from '@formium/client';
 
-export const formium = createClient(process.env.GATSBY_FORMIUM_PROJECTID);
+export const formium = createClient("600d70ebd0a32c0001f4a2e1");
